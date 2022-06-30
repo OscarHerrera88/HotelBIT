88ba0a5f83e8e60671be815921ace7585699c2b0
Oscar Herrera <oskr916@hotmail.com>
2022-06-29 21:37:43 -0500

    hotel

---
+++ js/contactenos.js
@@ -0,0 +1,11 @@
+function alerta() {
+    Swal.fire({
+        title: '¡Gracias por contactarnos!',
+        text: 'Viajero/a gracias por tu contacto, tendrás respuesta muy pronto.',
+        imageUrl: 'img/logo.png',
+        imageWidth: '220px',
+        imageHeight: '200px',
+        imageAlt: 'Logo',
+        confirmButtonColor: '#73CBD8',
+    })
+}
---
+++ js/reserva.js
@@ -0,0 +1,11 @@
+function alerta2() {
+    Swal.fire({
+        title: '¡Gracias por tu preferencia!',
+        text: 'Tu reserva se ha completado con éxito, prepárate para ser más felíz.',
+        imageUrl: 'img/logo2.png',
+        imageWidth: '220px',
+        imageHeight: '200px',
+        imageAlt: 'Logo2',
+        confirmButtonColor: '#73CBD8',
+    })
+}
